2f63d057f15b1103cdeb42b0fe89b9b846b7a013
DanielDSPh <95009275+DanielDSPh@users.noreply.github.com>
2022-12-02 20:40:53 -0600

    Más pruebas

--- arboles/src/arboles/NodoString.java
+++ arboles/src/arboles/NodoString.java
@@ -1,35 +1,67 @@
 package arboles;
 
 /**
- *
- * @author danie
+ * La clase modela un nodo con datos de tipos String
  */
 public class NodoString {
+    
+    /**
+     * Almacena el dato
+     */
     String dato;
+    
+    /**
+     * Hace referencia al hijo izquierdo
+     */
     NodoString izq;
+    
+    /**
+     * Hace referencia al hijo derecho
+     */
     NodoString der;
     
+    /**
+     * Método constructor del nodo
+     * @param s es el dato almacenado
+     */
     NodoString(String s){
         dato = s;
         izq = der = null;
     }
     
+    /**
+     * Asigna la referencia del nodo izquierdo
+     * @param nodo es el nodo hijo izquierdo
+     */
     public void setIzq(NodoString nodo){
         izq = nodo;
     }
     
+    /**
+     * Asigna la referencia del nodo derecho
+     * @param nodo es el nodo hijo derecho
+     */
     public void setDer(NodoString nodo){
         der = nodo;
     }
     
+    /**
+     * @return devuelve el hijo izquierdo
+     */
     public NodoString getIzq(){
         return izq;
     }
     
+    /**
+     * @return devuelve el hijo derecho
+     */
     public NodoString getDer(){
         return der;
     }
     
+    /**
+     * @return devuelve el dato del nodo
+     */
     public String getDato(){
         return dato;
     }
--- arboles/src/arboles/Principal.java
+++ arboles/src/arboles/Principal.java
@@ -15,7 +15,7 @@ public class Principal {
        arbolito2.postOrden();
        
        System.out.println("\n============");
-       ArbolExpArit arbolito3 = new ArbolExpArit("(()()())");
+       ArbolExpArit arbolito3 = new ArbolExpArit("4+2*(5-2)");
        arbolito3.postOrden();
     }
 }
